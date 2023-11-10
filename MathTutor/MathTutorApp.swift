ff81caee8b6ba66113ea1e83bd132f3eb4d097ab
David Veeneman <davidv@veeneman.com>
2023-11-10 13:47:33 -0800

    Initial Commit

---
+++ MathTutor/MathTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathTutorApp.swift
+//  MathTutor
+//
+//  Created by David Veeneman on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MathTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
