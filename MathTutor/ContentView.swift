a0d36b142f8db67b5a911b490dc5475c7fd474e1
David Veeneman <davidv@veeneman.com>
2023-11-10 16:11:55 -0800

    2023/11/10 - MathTutor Challenge: Initial commit; completed to 11:56 in video

--- MathTutor/ContentView.swift
+++ MathTutor/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var firstNumber = 0
+    @State private var secondNumber = 0
+    @State private var firstNumberEmojis = ""
+    @State private var secondNumberEmojis = ""
+    private let emojis = ["🍕", "🍎", "🍏", "🐵", "👽", "🧠", "🧜🏽‍♀️", "🧙🏿‍♂️", "🥷", "🐶", "🐹", "🐣", "🦄", "🐝", "🦉", "🦋", "🦖", "🐙", "🦞", "🐟", "🦔", "🐲", "🌻", "🌍", "🌈", "🍔", "🌮", "🍦", "🍩", "🍪"]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Group {
+                Text(firstNumberEmojis)
+                Text("+")
+                Text(secondNumberEmojis)
+            }
+            .font(.system(size: 80))
+            .minimumScaleFactor(0.5)
+            .multilineTextAlignment(.center)
+            Spacer()
+            
+            Text("\(firstNumber) + \(secondNumber)")
+                .font(.largeTitle)
+            
+            Spacer()
         }
         .padding()
+        .onAppear {
+            firstNumber = Int.random(in:1...10)
+            secondNumber = Int.random(in:1...10)
+            firstNumberEmojis = setupEmojiString(number: firstNumber)
+            secondNumberEmojis = setupEmojiString(number: secondNumber)
+        }
+    }
+    
+    func setupEmojiString(number:Int)-> String {
+        var emojiString = ""
+        
+        // Long version
+        //        let randomEmoji = emojis.randomElement()!
+        //        for _ in 1...number {
+        //            emojiString += randomEmoji
+        //        }
+        
+        //Short version
+        emojiString = String(repeating: emojis.randomElement()!, count: number)
+        
+        return emojiString
     }
 }
 
